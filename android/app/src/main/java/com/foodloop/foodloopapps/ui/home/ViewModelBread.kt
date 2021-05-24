01015ca283ccb6ba43bfa429bdd0b219002efa98
AdeeeW <adewijayanto2@gmail.com>
2021-05-24 23:05:04 +0700

    foodloop

--- android/app/src/main/java/com/foodloop/foodloopapps/ui/home/ViewModelBread.kt
+++
@@ -1,9 +0,0 @@
-package com.foodloop.foodloopapps.ui.home
-
-import androidx.lifecycle.ViewModel
-import com.foodloop.foodloopapps.data.entity.BreadEntity
-import com.foodloop.foodloopapps.utils.Data
-
-class ViewModelBread: ViewModel() {
-    fun getMovies(): List<BreadEntity> = Data.generateDataBread()
-}
