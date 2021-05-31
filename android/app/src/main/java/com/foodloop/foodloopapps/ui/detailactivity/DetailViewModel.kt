db595c4627ae497d2d5113810f76263456f1a260
Martin Hizkia <martinhizkia@gmail.com>
2021-05-31 14:21:58 +0700

    merge

--- android/app/src/main/java/com/foodloop/foodloopapps/ui/detailactivity/DetailViewModel.kt
+++ android/app/src/main/java/com/foodloop/foodloopapps/ui/detailactivity/DetailViewModel.kt
@@ -9,7 +9,6 @@ import com.foodloop.foodloopapps.data.entity.BreadEntity
 import com.foodloop.foodloopapps.data.network.ApiConfig
 import com.foodloop.foodloopapps.data.network.ApiService
 import com.foodloop.foodloopapps.data.respons.InfoDetailRespons
-import com.foodloop.foodloopapps.data.respons.ResultInfoRespons
 import com.foodloop.foodloopapps.utils.Data
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,22 +20,22 @@ class DetailViewModel : ViewModel() {
         val home = ApiConfig.getApiService(BuildConfig.INFO_URL).create(
             ApiService::class.java
         )
-        home.getInfo().enqueue(object : Callback<ResultRespons> {
-            override fun onResponse(
-                call: Call<ResultRespons>,
-                response: Response<ResultRespons>
-            ) {
-                if (response.isSuccessful) {
-                    food.postValue(response.body().result)
-//                    Log.wtf("result0", response.body().toString())
-                }
-            }
-
-            override fun onFailure(call: Call<ResultRespons>, t: Throwable) {
-                Log.e("LOGIN", "Failed: ${t.message.toString()}")
-            }
-
-        })
+//        home.getInfo().enqueue(object : Callback<ResultRespons> {
+//            override fun onResponse(
+//                call: Call<ResultRespons>,
+//                response: Response<ResultRespons>
+//            ) {
+//                if (response.isSuccessful) {
+//                    food.postValue(response.body().result)
+////                    Log.wtf("result0", response.body().toString())
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<ResultRespons>, t: Throwable) {
+//                Log.e("LOGIN", "Failed: ${t.message.toString()}")
+//            }
+//
+//        })
     }
     fun getFoodDetail(): LiveData<InfoDetailRespons> {
         return food
